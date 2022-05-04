7c791f85266308320c660e6a336ea69894d1111d
StevanFreeborn <sfreeborn7@gmail.com>
2022-05-04 09:08:35 -0500

    update README.md

--- src/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-record = Record(195,fields)
-
-response = client.AddOrUpdateRecord(record)
-
-print(f'Status Code: {response.statusCode}')
-print(f'Id: {response.data.id}')
-for warning in response.data.warnings:
-    print(f'Warning: {warning}')
